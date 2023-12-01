e505aebc610b5fd2a0a05828d32d5b3c876c3ec1
ivanchikk <vanyaklochkoff@gmail.com>
2023-12-01 06:25:51 +0200

    Refactored dto's validation

--- src/main/java/com/example/gymapi/web/dto/user/UserCreationDto.java
+++ src/main/java/com/example/gymapi/web/dto/user/UserCreationDto.java
@@ -6,30 +6,26 @@ import lombok.Data;
 
 @Data
 public class UserCreationDto {
-    @NotNull
     @NotBlank(message = "FirstName is required")
     @Pattern(regexp = "^[a-zA-Z]+$", message = "Latin letters only")
     @Size(max = 32, message = "32 characters are maximum")
     private String firstName;
 
-    @NotNull
     @NotBlank(message = "LastName is required")
     @Pattern(regexp = "^[a-zA-Z]+$", message = "Latin letters only")
     @Size(max = 32, message = "32 characters are maximum")
     private String lastName;
 
-    @NotNull
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email")
     private String email;
 
-    @NotNull
     @NotBlank(message = "Password is required")
     @Size(min = 8, message = "8 characters are required")
     @Size(max = 32, message = "32 characters are maximum")
     private String password;
 
-    @NotNull
-    @Pattern(regexp = "^\\+38\\(0[0-9]{2}\\)[0-9]{3}-[0-9]{2}-[0-9]{2}$")
+    @NotBlank(message = "Phone number is required")
+    @Pattern(regexp = "^\\+38\\(0[0-9]{2}\\)[0-9]{3}-[0-9]{2}-[0-9]{2}$", message = "Invalid phone number")
     private String phoneNumber;
 }
--- src/main/java/com/example/gymapi/web/dto/user/UserUpdateDto.java
+++ src/main/java/com/example/gymapi/web/dto/user/UserUpdateDto.java
@@ -6,24 +6,21 @@ import lombok.Data;
 
 @Data
 public class UserUpdateDto {
-    @NotNull
     @NotBlank(message = "FirstName is required")
     @Pattern(regexp = "^[a-zA-Z]+$", message = "Latin letters only")
     @Size(max = 32, message = "32 characters are maximum")
     private String firstName;
 
-    @NotNull
     @NotBlank(message = "LastName is required")
     @Pattern(regexp = "^[a-zA-Z]+$", message = "Latin letters only")
     @Size(max = 32, message = "32 characters are maximum")
     private String lastName;
 
-    @NotNull
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email")
     private String email;
 
-    @NotNull
-    @Pattern(regexp = "^\\+38\\(0[0-9]{2}\\)[0-9]{3}-[0-9]{2}-[0-9]{2}$")
+    @NotBlank(message = "Phone number is required")
+    @Pattern(regexp = "^\\+38\\(0[0-9]{2}\\)[0-9]{3}-[0-9]{2}-[0-9]{2}$", message = "Invalid phone number")
     private String phoneNumber;
 }
